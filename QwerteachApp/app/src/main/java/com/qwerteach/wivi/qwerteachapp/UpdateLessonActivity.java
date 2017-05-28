e27027d68801ff0ee8f22964a1f21113bfa6dda8
Virginie Bosquet <vivi_sander@hotmail.com>
2017-05-28 22:02:49 +0200

    Modifying My Lessons Activity

--- QwerteachApp/app/src/main/java/com/qwerteach/wivi/qwerteachapp/UpdateLessonActivity.java
+++ QwerteachApp/app/src/main/java/com/qwerteach/wivi/qwerteachapp/UpdateLessonActivity.java
@@ -44,7 +44,7 @@ public class UpdateLessonActivity extends AppCompatActivity implements View.OnCl
         TimePickerDialog.OnTimeSetListener, DatePickerDialog.OnDateSetListener {
 
     Lesson lesson;
-    TextView topicGroupTextView, topicTextView, levelTextView, totalPriceTextView, lessonDurationTextView, dateTextView, timeTextView;
+    TextView dateTextView, timeTextView;
     Button datePickerButton, timePickerButton, saveLessonInfosButton;
     ProgressDialog progressDialog;
     QwerteachService service;
@@ -52,7 +52,6 @@ public class UpdateLessonActivity extends AppCompatActivity implements View.OnCl
     TimePickerDialog timePickerDialog;
     DatePickerDialog datePickerDialog;
     Date newDate;
-    Calendar now;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,29 +75,18 @@ public class UpdateLessonActivity extends AppCompatActivity implements View.OnCl
         service = ApiClient.getClient().create(QwerteachService.class);
 
         newDate = new Date(System.currentTimeMillis());
-        now = Calendar.getInstance();
-        timePickerDialog = TimePickerDialog.newInstance(this, now.get(Calendar.HOUR_OF_DAY), now.get(Calendar.MINUTE), true);
-        datePickerDialog = DatePickerDialog.newInstance(this, now.get(Calendar.YEAR), now.get(Calendar.MONTH), now.get(Calendar.DAY_OF_MONTH));
+        timePickerDialog = TimePickerDialog.newInstance(this, lesson.getHour(), lesson.getMinute(), true);
+        datePickerDialog = DatePickerDialog.newInstance(this, lesson.getYear(), lesson.getMonth(), lesson.getDay());
         datePickerDialog.setMinDate(java.util.Calendar.getInstance());
 
-        topicGroupTextView = (TextView) findViewById(R.id.lesson_topic_group_text_view);
-        topicTextView = (TextView) findViewById(R.id.lesson_topic_text_view);
-        levelTextView = (TextView) findViewById(R.id.lesson_level_text_view);
-        totalPriceTextView = (TextView) findViewById(R.id.total_price_text_view);
-        lessonDurationTextView = (TextView) findViewById(R.id.lesson_duration_text_view);
         dateTextView = (TextView) findViewById(R.id.date_picker_text_view);
         timeTextView = (TextView) findViewById(R.id.time_picker_text_view);
         datePickerButton = (Button) findViewById(R.id.date_picker_button);
         timePickerButton = (Button) findViewById(R.id.time_picker_button);
         saveLessonInfosButton = (Button) findViewById(R.id.save_lesson_infos_button);
 
-        dateTextView.setText(lesson.getDate(lesson.getTimeStart()));
-        timeTextView.setText(lesson.getTime(lesson.getTimeStart()));
-        topicTextView.setText(lesson.getTopicTitle());
-        topicGroupTextView.setText(lesson.getTopicGroupTitle());
-        levelTextView.setText(lesson.getLevel());
-        totalPriceTextView.setText(lesson.getPrice() + " €");
-        lessonDurationTextView.setText(lesson.getDuration());
+        dateTextView.setText(lesson.getDate());
+        timeTextView.setText(lesson.getTime());
 
         datePickerButton.setOnClickListener(this);
         timePickerButton.setOnClickListener(this);
